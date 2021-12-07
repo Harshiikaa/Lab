0435186d36fc42edb47b81f9b4afdd41e59d8d62
Harshiikaa <93309678+Harshiikaa@users.noreply.github.com>
2021-12-07 16:41:03 +0545

    Delete Day2 directory

--- Day2/Lab5.py
+++
@@ -1,3 +0,0 @@
-# A school decided to replace the desks in three classrooms. Each desk sits two students.
-# Given the number of  students in each class, print the smallest possible number of desks that can be purchased.
-# The program should read three integers: the number of students in each of the three classes, a,b and c respectively.
